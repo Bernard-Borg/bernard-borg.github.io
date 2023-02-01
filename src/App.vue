ffc42595d4828bd13e6e79ae17c01582fcbeea35
Bernard Borg <bernard.borg36@gmail.com>
2023-02-01 22:15:41 +0100

    Made homepage responsive

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import { useTimeoutFn } from "@vueuse/shared";
+import "animate.css";
 import { ref } from "vue";
 import SectionSelector from "./components/SectionSelector.vue";
 import Socials from "./components/Socials.vue";
 
 const focusedSection = ref<string>("");
+const clickCounter = ref<number>(0);
+const profilePicture = ref<HTMLImageElement>();
 
 const handleFocus = (section: string) => {
     focusedSection.value = section;
@@ -12,23 +16,55 @@ const handleFocus = (section: string) => {
 const handleFocusLoss = () => {
     focusedSection.value = "";
 };
+
+const playGame = () => {};
+
+const clickPicture = () => {
+    if (clickCounter.value >= 5) {
+        playGame();
+    } else {
+        if (profilePicture.value?.classList.contains("animate__headShake")) {
+            profilePicture.value?.classList.remove("animate__headShake");
+        }
+
+        useTimeoutFn(() => {
+            profilePicture.value?.classList.add("animate__headShake");
+        }, 50);
+    }
+
+    clickCounter.value++;
+};
 </script>
 
 <template>
     <Socials />
     <div class="flex flex-col mb-10">
-        <div class="flex w-full justify-evenly mt-20">
+        <div
+            class="flex flex-col sm:items-center md:flex-row w-full justify-evenly mt-20"
+        >
             <img
-                class="rounded-full aspect-square object-cover"
-                style="width: 350px"
+                ref="profilePicture"
+                id="profilePicture"
+                class="rounded-full aspect-square object-cover animate__animated transition-all duration-300"
                 src="https://media.licdn.com/dms/image/C4D03AQFENYez-mpIQg/profile-displayphoto-shrink_800_800/0/1642376010470?e=1680739200&v=beta&t=lim4dPd3TnEjIiFxqij_JSk_6vc_hP2QI5UCtpAShI4"
+                @click="clickPicture"
             />
-            <div class="flex flex-col justify-center">
-                <h1 class="text-9xl font-bold">Bernard Borg</h1>
-                <h2 class="text-5xl font-thin">Fullstack Software Developer</h2>
+            <div class="flex flex-col justify-center mt-10 md:mt-0">
+                <h1
+                    class="text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold transition-all duration-300"
+                >
+                    Bernard Borg
+                </h1>
+                <h2
+                    class="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-thin text-center md:text-left transition-all duration-300"
+                >
+                    Fullstack Software Developer
+                </h2>
             </div>
         </div>
-        <div class="grid grid-cols-3 pt-20 px-12 gap-16">
+        <div
+            class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 pt-20 px-12 gap-16"
+        >
             <SectionSelector
                 title="About Me"
                 icon="fa-solid fa-circle-user"
@@ -83,4 +119,26 @@ const handleFocusLoss = () => {
 
 <style>
 @import "@fortawesome/fontawesome-free/css/all.min.css";
+
+#profilePicture {
+    width: 350px;
+}
+
+@media screen and (max-width: 1279px) {
+    #profilePicture {
+        width: 250px;
+    }
+}
+
+@media screen and (max-width: 1023px) {
+    #profilePicture {
+        width: 200px;
+    }
+}
+
+@media screen and (max-width: 767px) {
+    #profilePicture {
+        width: 350px;
+    }
+}
 </style>
