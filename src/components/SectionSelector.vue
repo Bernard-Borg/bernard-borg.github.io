e37a43c8108bb8a5c239f9710f89c15f2462e089
Bernard Borg <bernard.borg36@gmail.com>
2023-02-01 22:15:30 +0100

    Added animations to sections

--- src/components/SectionSelector.vue
+++ src/components/SectionSelector.vue
@@ -16,39 +16,55 @@ const props = defineProps({
 onClickOutside(card, () => emit("lost-focus"));
 onKeyStroke(["Escape"], () => emit("lost-focus"));
 
-const parentClassesWhenNotFocused = "hover:scale-110";
-const parentClassesWhenFocused = "fixed top-0 right-0 left-0 backdrop-blur-md";
-const classesWhenNotFocused = "";
-const classesWhenFocused = "m-12 shadow-xl shadow-stone-500";
-
 const isFocused = computed(() => props.focusedSection === props.sectionId);
 </script>
 
 <template>
-    <div
-        :class="`${
-            isFocused ? parentClassesWhenFocused : parentClassesWhenNotFocused
-        }`"
-        :style="`height: ${isFocused ? '100vh' : ''}`"
-    >
+    <div class="hover:scale-110">
         <div
-            ref="card"
-            :class="`box-border flex flex-col bg-white text-black rounded p-8 transition-transform gap-y-3 ${
-                isFocused ? classesWhenFocused : classesWhenNotFocused
-            }`"
-            :style="`height: ${isFocused ? 'calc(100% - 6rem)' : '250px'}`"
+            class="box-border flex flex-col bg-white text-black rounded p-8 transition-transform gap-y-3"
+            style="height: 250px"
             @click="$emit('focused', sectionId)"
-            @onClickOutside="$emit('lost-focus', sectionId)"
         >
             <div class="flex">
                 <h3 class="font-bold text-2xl">{{ title }}</h3>
                 <i :class="`text-2xl ml-3 ${icon}`"></i>
             </div>
             <div class="border-solid border-t-2 border-black">
-                <slot></slot>
+                <slot name="summary-content"></slot>
             </div>
         </div>
     </div>
+    <Transition
+        enter-active-class="animate__animated animate__fadeIn"
+        leave-active-class="animate__animated animate__fadeOut"
+    >
+        <div
+            v-if="isFocused"
+            class="fixed top-0 right-0 left-0 backdrop-blur-md"
+            style="height: 100vh; z-index: 100"
+        >
+            <div
+                ref="card"
+                class="box-border flex flex-col bg-white text-black rounded p-8 transition-transform gap-y-3 m-12 shadow-xl shadow-stone-500"
+                style="height: calc(100% - 6rem)"
+                @onClickOutside="$emit('lost-focus', sectionId)"
+            >
+                <div class="flex">
+                    <h3 class="font-bold text-2xl">{{ title }}</h3>
+                    <i :class="`text-2xl ml-3 ${icon}`"></i>
+                </div>
+                <div class="border-solid border-t-2 border-black">
+                    <slot name="full-content"></slot>
+                </div>
+            </div>
+        </div>
+    </Transition>
 </template>
 
-<style></style>
+<style>
+.animate__animated.animate__fadeIn,
+.animate__animated.animate__fadeOut {
+    --animate-duration: 0.5s;
+}
+</style>
