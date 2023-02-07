dbdc037fe4ec6e4b3babc311e1fe30ebabb42421
Bernard Borg <bernard.borg36@gmail.com>
2023-02-08 00:30:05 +0100

    Added final-ish section of About Me

--- src/components/sections/AboutMe.vue
+++ src/components/sections/AboutMe.vue
@@ -28,21 +28,38 @@ const yearsOfLearning = computed(() => {
             /> </HoverLink
         >. I have been learning Computing/Software Development for the past {{ yearsOfLearning }} years. My journey into
         the world of software started out with a love for Java. The first thing I got obsessed with was JOptionPanes. I
-        used to make use of them in all my projects, soon after learning about more advanced GUI technologies such as
+        used to make use of them in all my projects. Soon after, I learnt about more advanced GUI technologies such as
         AWT and Swing. I enjoyed tinkering around, creating small programs, eventually creating a small
         <HoverLink text="2D maze game"><img src="/BAmazed.png" class="w-96" /></HoverLink>
         for my MATSEC A-level project (which I completed through private lessons, while studying Chemistry and Pure
         Mathematics for my A-levels).
     </p>
-    <p class="text-lg leading-7 text-justify">
+    <p class="text-lg leading-7 text-justify mb-3">
         Later on, I started completing online courses on various technologies, namely Java and Kotlin (through
         <HoverLink text="Hyperskill.org" link="https://hyperskill.org/tracks" :openInNewTab="true" />, which I can't
-        recommend enough), as well as Javascript, HTML, CSS, Python and react (initially through
+        recommend enough), as well as Javascript, HTML, CSS, Python and React (initially through
         <HoverLink text="Codecademy" link="https://www.codecademy.com/" :openInNewTab="true" />, and finally through
         <HoverLink text="freeCodeCamp" link="https://www.freecodecamp.org/" :openInNewTab="true"
             ><img src="/freeCodeCampCert.png" class="w-96" /></HoverLink
         >).
     </p>
+    <p class="text-lg leading-7 text-justify mb-3">
+        During this time, I was also in the process of getting my Bachelor of Science in IT (Software Development) degree
+        at the University of Malta, while also being part of multiple student placement internships. I have now graduated with a
+        first-class honours and am working full-time at <HoverLink text="Megabyte" link="https://www.megabyte.net/" :openInNewTab="true">
+            <img src="/megabyte-logo.png" class="w-28" />
+        </HoverLink>. 
+        I'm currently looking to get a diploma in UI/UX design, enjoy contributing on 
+        <HoverLink text="StackOverflow" link="https://stackoverflow.com/users/13426975/bernard-borg" :openInNewTab="true"></HoverLink> 
+        and love to create some <HoverLink text="personal projects" link="https://github.com/Bernard-Borg/" :openInNewTab="true"></HoverLink> 
+        when I'm bored.
+    </p>
+    <p class="text-lg leading-7 text-justify mb-3">Currently enjoying the following technologies;</p>
+    <ul class="list-disc ml-10">
+        <li>Vue 3 (Composition API with <span class="font-mono" style="color: #1e50b3">{{ '<script setup>' }}</span> + Pinia + Vite)</li>
+        <li>TailwindCSS / Bootstrap 5</li>
+        <li>.NET Core 6</li>
+    </ul>
 </template>
 
 <style>
